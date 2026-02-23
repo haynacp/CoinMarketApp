d25b39ff1d3a33b8122d4e3a0d28c25e2d2ebce2
Hayna Cardoso <haynapinto@cartaodetodos.com>
2026-02-23 11:52:58 -0300

    Initial Commit

---
+++ CoinMarketApp/CoinMarketAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoinMarketAppApp.swift
+//  CoinMarketApp
+//
+//  Created by Hayna Cardoso on 23/02/26.
+//
+
+import SwiftUI
+
+@main
+struct CoinMarketAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CoinMarketApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CoinMarketApp
+//
+//  Created by Hayna Cardoso on 23/02/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
